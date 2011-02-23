0da0d5757300b5f59957ca603fb8aa6d16ac5023
Jonas Ådahl <jadahl@gmail.com>
2011-02-23 08:05:54 +0100

    Fix for when command results are lists. (from funorpain @ github)

--- src/mod_restful_admin.erl
+++ src/mod_restful_admin.erl
@@ -201,8 +201,10 @@ format_result_json(Code, {_, rescode}) ->
     Code;
 format_result_json({Code, Text}, {_, restuple}) ->
     [{Code, list_to_binary(Text)}];
-format_result_json([Es], {_, {list, ElementsF}}) ->
-    [format_result_json(E, ElementsF) || E <- Es];
+format_result_json([E], {_, {list, ElementsF}}) ->
+    [format_result_json(E, ElementsF)];
+format_result_json([E|T], {X, {list, ElementsF}}) ->
+    [format_result_json(E, ElementsF) | format_result_json(T, {X, {list, ElementsF}}) ];
 format_result_json(Tuple, {_, {tuple, ElementsF}}) ->
     TupleL = tuple_to_list(Tuple),
     % format a tuple as a list
